92a250d249df023eab1f6756636a10e2cbf40f4f
Thai Tuan An <dangdraver777@gmail.com>
2025-03-21 00:30:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cameraapi/MainCamera.java
@@ -0,0 +1,28 @@
+package com.example.cameraapi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_camera);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Python.start(new AndroidPlatform(getApplicationContext()));
+            return insets;
+        });
+    }
+}
